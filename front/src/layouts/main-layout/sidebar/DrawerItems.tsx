deaba3cc8c88bbde986f345bbe1fb26c4fc08608
ami kahlon <amikahlon2@gmail.com>
2025-01-18 01:41:07 +0200

    new logo

--- front/src/layouts/main-layout/sidebar/DrawerItems.tsx
+++ front/src/layouts/main-layout/sidebar/DrawerItems.tsx
@@ -1,55 +1,56 @@
-import { fontFamily } from 'theme/typography';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import ButtonBase from '@mui/material/ButtonBase';
-import Typography from '@mui/material/Typography';
 import ListItem from './list-items/ListItem';
 import CollapseListItem from './list-items/CollapseListItem';
 import sitemap from 'routes/sitemap';
+import logo from '../../../../public/logo.svg'
 
 const DrawerItems = () => {
   return (
     <>
-      <Stack
-        pt={5}
-        pb={3.5}
-        px={4.5}
-        position="sticky"
-        top={0}
-        bgcolor="info.light"
-        alignItems="center"
-        justifyContent="flex-start"
-        borderBottom={1}
-        borderColor="info.main"
-        zIndex={1000}
-      >
-        <ButtonBase component={Link} href="/" disableRipple>
-          <Box>
-            <Typography
-              mt={0.25}
-              variant="h3"
-              color="primary.main"
-              textTransform="uppercase"
-              letterSpacing={1}
-              fontFamily={fontFamily.poppins}
-            >
-              Venus
-            </Typography>
-            <Typography
-              mt={-0.35}
-              variant="body2"
-              color="primary.main"
-              textTransform="uppercase"
-              fontWeight={500}
-              fontFamily={fontFamily.poppins}
-            >
-              Dashboard
-            </Typography>
-          </Box>
-        </ButtonBase>
+<Stack
+  pt={2}
+  pb={2}
+  px={4.5}
+  position="sticky"
+  top={2}
+  bgcolor="info.light"
+  alignItems="center" 
+  justifyContent="center" 
+  borderBottom={1}
+  borderColor="info.main"
+  zIndex={1000}
+>
+<ButtonBase
+  component={Link}
+  href="/"
+  disableRipple
+  sx={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '80px', 
+    width: '100%', 
+  }}
+>
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <img
+      src={logo}
+      alt="Venus Dashboard Logo"
+      style={{ height: '60px', objectFit: 'contain' }}
+    />
+  </Box>
+</ButtonBase>
       </Stack>
 
       <List component="nav" sx={{ mt: 2.5, mb: 10, px: 4.5 }}>
