ee579b7f204b02ef98df6c77c74291829ffb8f15
Guy Mishan <gaiu01999@gmail.com>
2025-03-17 16:24:10 +0200

    finished server production + https

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,29 @@
 import { app, connectToDatabase } from "./server";
+import https from "https"
+import fs from "fs"
 
 const PORT = process.env.PORT || 8000;
 
 // חיבור ל-MongoDB
 connectToDatabase()
   .then(() => {
-    // הפעלת השרת לאחר שהחיבור הצליח
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+    // // הפעלת השרת לאחר שהחיבור הצליח
+    // app.listen(PORT, () => {
+    //   console.log(`Server is running on http://localhost:${PORT}`);
+    // });
+    console.log("Environment:" + process.env.NODE_ENV)
+    if (process.env.NODE_ENV != "production") {
+      app.listen(PORT, () => {
+        console.log(`Example app listening at http://localhost:${PORT}`);
+      });
+    } else {
+      console.log(`app listening at https://193.106.55.221`);
+      const prop = {
+        key: fs.readFileSync("./client-key.pem"),
+        cert: fs.readFileSync("./client-cert.pem")
+      }
+      https.createServer(prop, app).listen(PORT)
+    }
   })
   .catch((err) => {
     console.error(
